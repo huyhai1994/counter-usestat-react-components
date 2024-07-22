2690370e7c2a6a29af0d3fe161c4106428f82fc7
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-07-22 13:31:09 +0700

    update project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import './App.css';
+import {useState} from 'react';
 
 function App() {
-    return (<div></div>);
+    /* Code function useIncrement() that takes addAmount as a parameter, which is the unit of increase for each click
+       Initialize state: const [count, setCount] = useState(0);
+       Create a function to increment the count: function increment(), which takes addAmount as a parameter. Perform setCount
+       Use the return statement [count, increment] to return values for Counter 1 and Counter 2 components
+    */
+    const useIncrement = (addAmount) => {
+        const [count, setCount] = useState(0);
+        const increment = () => setCount(count + addAmount);
+        return [count, increment];
+    }
+
+    /* Create Counter 1 component
+       Initialize const [count, setCount] = useIncrement(1);
+       Create UI elements
+         Handle button event and call setCount: onClick={increment}
+    */
+    const Counter1 = () => {
+        const [count, increment] = useIncrement(1);
+        return (
+            <div>Count: {count} <br/>
+                <button onClick={increment}>Increase</button>
+            </div>
+
+        );
+    }
+
+    return (<Counter1/>);
 }
 
 export default App;
